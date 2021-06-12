d92dd5b427941dedb8e6379ba4059a946b8d6fc1
hardikchopra242 <hardikchopra242@gmail.com>
2021-06-13 00:21:07 +0530

    [New] New animations on SignIn & Register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -44,7 +44,7 @@ class Register extends React.Component {
 
   render() {
     return (
-      <article className="animate__animated animate__fadeInDown br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+      <article className="animate__animated animate__zoomIn  br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
           <div className="measure">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../../../src/App.css';
 import '../Register/Register.css'
+
 class Signin extends React.Component {
   constructor(props) {
     super(props);
@@ -39,7 +40,7 @@ class Signin extends React.Component {
   render() {
     const { onRouteChange } = this.props;
     return (
-      <article className="animate__animated animate__fadeInRight br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+      <article className="animate__animated animate__flipInY  br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
           <div className="measure">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
